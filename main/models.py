d24d5cd1820030f7e3efc23e083fa509018e5b42
KarenWanyama3430 <karenwanyama3430@gmail.com>
2024-02-08 14:00:48 +0300

    changes to models

--- main/models.py
+++ main/models.py
@@ -6,12 +6,12 @@ from django.db import models
 
 
 
-class Course(models.Model):
-    name=models.CharField(max_length=1000)
-    description=models.TextField(default ='N/A',blank=True,null=True)
-    date_created=models.DateTimeField(auto_now_add=True,blank=True,null=True)
-    date_modified=models.DateTimeField(auto_now=True,blank=True,null=True)
+# class Course(models.Model):
+#     name=models.CharField(max_length=1000)
+#     description=models.TextField(default ='N/A',blank=True,null=True)
+#     date_created=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+#     date_modified=models.DateTimeField(auto_now=True,blank=True,null=True)
 
 
-    def_str_(self):
-        return f"{self.name}"
+#     def_str_(self):
+#         return f"{self.name}"
